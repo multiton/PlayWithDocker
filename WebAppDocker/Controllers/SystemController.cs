f3d860345c5815e7fcae6ccbba86446870c48859
multiton <k_smithus@yahoo.com>
2023-06-07 20:59:35 -0400

    SysyemController added

---
+++ WebAppDocker/Controllers/SystemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]    
+    public class SystemController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok(Environment.OSVersion);
+    }
+}
